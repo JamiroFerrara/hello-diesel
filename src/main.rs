cbc044a424e7637be5afc1d13df77a5c11b81cc1
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-08-17 19:35:03 +0200

    Implemented Rocketù

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 extern crate diesel;
 
-use self::diesel::prelude::*;
-use self::models::*;
+#[macro_use] extern crate rocket;
 
-fn main() {
-    use schema::Track::dsl::*;
-    let connection = db::establish_connection();
-    let results = hello_diesel::schema::track.load::<Track>(&connection) .expect("Error loading tracks");
+use self::diesel::prelude::*;
+use hello_diesel::models::*;
+
+mod db;
+
+#[launch]
+async fn rocket() -> _ {
+    #[get("/Artists")]
+    async fn artists() -> String {
+        use hello_diesel::schema::Artist::dsl::*;
+
+        let connection = db::establish_connection();
+        let artists = Artist.load::<hello_diesel::models::Artist>(&connection);
+        match artists {
+            Ok(artists) => format!("{:?}", artists),
+            Err(e) => format!("Error: {}", e),
+        }
+    }
+
+    rocket::build()
+        .mount("/", routes![artists])
 }
